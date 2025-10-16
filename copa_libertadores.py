915bb21ca1746b73937c291a07d3d9a399f981ef
Daniel Dezan Lopes da Silva <danieldezan@hotmail.com>
2025-10-16 16:08:20 -0300

    Melhora impressão na tela

--- copa_libertadores.py
+++ copa_libertadores.py
@@ -2,8 +2,9 @@ import pandas as pd
 import requests
 from io import StringIO
 import re
+import ast
 
-url = "https://pt.wikipedia.org/wiki/Copa_Libertadores_da_Am%C3%A9rica"
+url = 'https://pt.wikipedia.org/wiki/Copa_Libertadores_da_Am%C3%A9rica'
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
 }
@@ -15,7 +16,8 @@ dfs = pd.read_html(StringIO(response.text))
 df_temp = dfs[11]
 df_final = df_temp[['País', 'Títulos']].copy()
 
-print("Versão inicial da tabela")
+
+print('\nVersão inicial da tabela\n')
 print(df_final)
 
 # Função para extrair o número de títulos
@@ -38,5 +40,22 @@ df_final = df_final[~df_final['País'].str.contains('Em caso de empate')]
 # Remover a coluna 'Numero_Titulos' se não for necessária
 df_final = df_final[['País', 'Títulos']]
 
-print("Versão tratada da tabela")
+print('\n-----------------------\n')
+print('Versão tratada da tabela')
 print(df_final)
+
+# Remover o número de títulos e extrair os anos
+df_final['Títulos'] = df_final['Títulos'].str.replace(r'^\d+\s*\((.*)\)$', r'\1', regex=True).copy()
+
+# Agrupar os anos por país
+df_grouped = df_final.assign(Títulos=df_final['Títulos'].str.replace(r'^\d+\s*\((.*)\)$', r'\1', regex=True)) \
+               .groupby('País')['Títulos'].apply(lambda x: ', '.join(x).split(', ')) \
+               .reset_index()
+
+# Remove as separações de anos com letra 'e' e garante que seja uma lista numérica
+df_grouped['Títulos'] = df_grouped['Títulos'].apply(lambda x: [item for sublist in [i.split(' e ') for i in x] for item in sublist])
+
+# Imprimir o resultado
+print('\n-----------------------\n')
+print('Tabela Agrupada')
+print(df_grouped)
